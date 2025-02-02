be5227e8b67810ed9a09a362748516fa06f6a138
veerababu558 <44125493+veerababu558@users.noreply.github.com>
2025-02-02 17:47:20 -0500

    Update ServiceB.java

--- src/main/java/com/example/demo/controller/ServiceB.java
+++ src/main/java/com/example/demo/controller/ServiceB.java
@@ -1,5 +1,10 @@
 package com.example.demo.controller;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +23,18 @@ public class ServiceB {
 	String getData() {
 		return "Calling from Service-A to Service B";
 		}
+	@GetMapping("/v1/ip")
+	 public Map<String, String> getIpAddress() {
+       Map<String, String> response = new HashMap<>();
+       try {
+           // Get the local IP address of the ECS task
+           String ipAddress = InetAddress.getLocalHost().getHostAddress();
+           response.put("ipAddress", ipAddress);
+       } catch (UnknownHostException e) {
+           response.put("error", "Failed to get IP address: " + e.getMessage());
+       }
+       return response;
+   }
 	
 	}
 	
